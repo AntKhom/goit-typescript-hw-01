9a8f4407e2d983fcc4209977dc8c77f92f87953b
AntKhom <antonhomenko1988@gmail.com>
2023-12-06 11:03:11 -0500

    Revert "ts"
    This reverts commit 29f88a7ba95be7774cbc9a522aa680dfce1ac54d.

--- dist/concatenation.js
+++
@@ -1,5 +0,0 @@
-function concatenation(firstword, secondword) {
-    console.log(`${firstword} ${secondword}`);
-}
-export { concatenation };
-//# sourceMappingURL=concatenation.js.map
--- src/concatenation.ts
+++
@@ -1,5 +0,0 @@
-function concatenation(firstword: string, secondword: string) {
-    console.log(`${firstword} ${secondword}`);
-}
-
-export {concatenation};
